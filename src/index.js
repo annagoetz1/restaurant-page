8a7e0cee9f6b7bcf4818f786a5c53d137ff06ee2
annagoetz1 <annalgoetz1@gmail.com>
2024-07-28 14:37:24 -0400

    correct newbutton and loafimage typo & consistency

---
+++ src/index.js
@@ -0,0 +1,26 @@
+
+
+import loafImageSrc from './loaf.jpg';
+import './styles.css'; // Import the CSS 
+
+console.log("Hello, Webpack!");
+
+let content = document.getElementById('content');
+let heading = document.createElement('h1');
+heading.textContent = 'Our Sourdough Bakery';
+
+let loafImage = document.createElement('img');
+loafImage.src = loafImageSrc;
+loafImage.alt = 'Sourdough Loaf on the counter';
+
+let secondHeader = document.createElement('h2');
+secondHeader.textContent = 'Welcome to my bakery! Where we sell sourdough loaves, sandwich breads, english muffins, bagels, and many different sourdough based pastries for optimal gut health and flavor. We sell at the Market District Farmers Market weekly during the summers from 11am-1pm on Thursdays. You can pre-order by clicking the button below to place an order for pickup and secure your items for the week!';
+ 
+let newButton = document.createElement('button');
+newButton.textContent = 'Preorders';
+newButton.id = 'preorder-button';
+
+content.appendChild(heading);
+content.appendChild(loafImage);
+content.appendChild(secondHeader);
+content.appendChild(newButton);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+};
